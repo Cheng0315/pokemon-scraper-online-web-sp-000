5c39ee9fd1b013fe5291ab1dcfcfe932a56a9c28
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 02:46:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,9 +2,9 @@ class Pokemon
   attr_accessor :name, :type, :db, :id
 
   def initialize(pokemon)
-    @id = pokemon
-    @name = pokemon
-    @type = pokemon
+    @id = pokemon[0]
+    @name = pokemon[1]
+    @type = pokemon[2]
   end
 
   def self.save(pk_name, pk_type, pk_db)
