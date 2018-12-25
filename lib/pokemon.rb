e5dbc3157338dbe7229180255b8b2c918f537618
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 02:20:10 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,10 +8,6 @@ class Pokemon
     pk_db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",pk_name, pk_type)
   end
 
-  def id
-    pk_db.execute("SELECT ? FROM pokemon WHERE pokemon.id = ?", pk_id)
-  end
-
   def self.find(pk_id, pk_db)
     pk_db.execute("SELECT #{pk_id} FROM pokemon WHERE pokemon.id = #{pk_id}")
   end
