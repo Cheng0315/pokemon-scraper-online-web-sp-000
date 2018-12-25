dc9e781c047669c4cff2197f1fa456a03e603239
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 02:37:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,15 @@
 class Pokemon
   attr_accessor :name, :type, :db, :id
-
+  @@all = []
   def initialize(pokemon)
     @id = pokemon[0]
     @name = pokemon[1]
     @type = pokemon[2]
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def self.save(pk_name, pk_type, pk_db)
@@ -14,5 +19,6 @@ class Pokemon
   def self.find(pk_id, pk_db)
     pokemon = pk_db.execute("SELECT * FROM pokemon WHERE pokemon.id = #{pk_id}")
     Pokemon.new(pokemon)
+    self.all.each {|pk| pk.id == pk_id}
   end
 end
