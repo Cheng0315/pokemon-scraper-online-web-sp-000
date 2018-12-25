979306294c573c2ba613967076cf4cc5362c63d5
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 03:08:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,6 @@ class Pokemon
     @id = pokemon[0]
     @name = pokemon[1]
     @type = pokemon[2]
-    @hp = 60
   end
 
   def self.save(pk_name, pk_type, pk_db)
@@ -16,6 +15,4 @@ class Pokemon
     pokemon = pk_db.execute("SELECT * FROM pokemon WHERE pokemon.id = #{pk_id}").flatten
     Pokemon.new(pokemon)
   end
-
-
 end
