2493543ca509dc9f27095efca3d0a1672b8a52d8
Cheng Vue <cvu3@ucsc.edu>
2018-12-25 02:04:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,15 @@
 class Pokemon
-  attr_accessor :id, :name, :type, :db
+  attr_accessor :name, :type, :db
 
   def initialize(db)
   end
 
   def self.save(pk_name, pk_type, pk_db)
     pk_db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",pk_name, pk_type)
+  end
+
+  def id
+    pk_db.execute("SELECT ? FROM pokemon WHERE pokemon.id = ?", pk_id)
   end
 
   def self.find(pk_id, pk_db)
